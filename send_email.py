adcec4b2a7ffd32a4dfede0ef45cdbd9e9577078
Marcela Saraí <marcelasaraigomezcanas@gmail.com>
2024-12-29 16:55:04 -0600

    feat: add happy new year email

--- send_email.py
+++ send_email.py
@@ -12,7 +12,7 @@ try:
     remitente = os.getenv('user')
     contraseña = os.getenv('password')
     destinatario = 'feraguilar6985@gmail.com'
-    asunto = 'Feliz navidad te desea DYGAV'
+    asunto = 'Feliz año nuevo te desea DYGAV'
 
     msg = MIMEMultipart()
 
@@ -20,7 +20,7 @@ try:
     msg['from'] = remitente
     msg['to'] = destinatario
 
-    with open('chirstmas_email.html', 'r') as archivo:
+    with open('new_year_email.html', 'r') as archivo:
         html = archivo.read()
 
     msg.attach(MIMEText(html, 'html',"utf-8"))
