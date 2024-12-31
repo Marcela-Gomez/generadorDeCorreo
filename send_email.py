2bc85b4abb010ace30bc00d783beaeccca2099be
Marcela Saraí <marcelasaraigomezcanas@gmail.com>
2024-12-31 14:35:55 -0600

    add logic for recipient list

--- send_email.py
+++ send_email.py
@@ -1,37 +1,46 @@
 import smtplib 
 import os 
+import pandas as pd
 from dotenv import load_dotenv
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import logging
 import logging.config
-
 load_dotenv()
+
+logging.basicConfig(
+    level=logging.ERROR,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+)
+logger = logging.getLogger('emailLogger')
 
 try:
     remitente = os.getenv('user')
     contraseña = os.getenv('password')
-    destinatario = 'feraguilar6985@gmail.com'
     asunto = 'Feliz año nuevo te desea DYGAV'
 
-    msg = MIMEMultipart()
-
-    msg['Subject'] = asunto
-    msg['from'] = remitente
-    msg['to'] = destinatario
-
-    with open('new_year_email.html', 'r') as archivo:
-        html = archivo.read()
-
-    msg.attach(MIMEText(html, 'html',"utf-8"))
+    data = pd.read_excel("data.xlsx")
+    destinatarios = data["email"].tolist()
 
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
     server.login(remitente, contraseña)
 
-    server.sendmail(remitente,destinatario,msg.as_string())
+    for destinatario in destinatarios:
+        msg = MIMEMultipart()
+
+        msg['Subject'] = asunto
+        msg['from'] = remitente
+        msg['to'] = destinatario
+
+        with open('new_year_email.html', 'r') as archivo:
+            html = archivo.read()
+
+        msg.attach(MIMEText(html, 'html', 'utf-8'))
+
+        server.sendmail(remitente,destinatario,msg.as_string())
 
     server.quit()
+    logger.info('Emails enviados con éxito')
 except Exception as e:
-    logger = logging.getLogger('simpleExample')
-    logger.error("Error: El correo no se envio")
+    logger.error("Error: El correo no se envio" + str(e))
